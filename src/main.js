d4f442a5828391a19132eeaea00d73a1db0f1c2d
Valera Vilks <valeravilks@gmail.com>
2019-10-05 07:57:42 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './style.scss';
+
+
+alert(1);
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let conf = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'script.js',
+        publicPath: "dist"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            'window.jQuery': 'jquery'
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.exec\.jjs$/,
+                use: [ 'script-loader' ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        }
+                    },
+                    "css-loader", // translates CSS into CommonJS
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name][hash].[ext]',
+                            limit: 8192
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'font',
+                            name: '[name].[ext]',
+                        },
+                    },
+                ]
+            }
+        ]
+    }
+};
+
+
+module.exports = conf;
